7907a0ac656ee6167e3143d980521a036894366c
Ben Steel <bendavidsteel@gmail.com>
2022-10-17 15:39:25 -0400

    Added final touches for node classification

--- assignment3/scripts/node_classification.py
+++ assignment3/scripts/node_classification.py
@@ -1,47 +1,58 @@
-from pkgutil import get_data
 import utils
 
+import numpy as np
 import torch
 import torch.nn.functional as F
-from torch_geometric.nn import GCNConv
+from torch_geometric.nn.models import GCN, GAT
 
 
-
-class GCN(torch.nn.Module):
-    def __init__(self, dataset):
+class GraphModel(torch.nn.Module):
+    def __init__(self, model_name, dataset):
         super().__init__()
-        self.conv1 = GCNConv(dataset.num_node_features, 16)
-        self.conv2 = GCNConv(16, dataset.num_classes)
+        in_feats = dataset.num_node_features
+        out_feats = dataset.num_classes
+        hidden_channels = 16
+        num_layers = 2
+        if model_name == 'gcn':
+            self.model = GCN(in_feats, hidden_channels, num_layers, out_channels=out_feats)
+        elif model_name == 'gat':
+            self.model = GAT(in_feats, hidden_channels, num_layers, out_channels=out_feats, v2=True)
 
     def forward(self, data):
         x, edge_index = data.x, data.edge_index
-
-        x = self.conv1(x, edge_index)
-        x = F.relu(x)
-        x = F.dropout(x, training=self.training)
-        x = self.conv2(x, edge_index)
+        x = self.model(x, edge_index)
 
         return F.log_softmax(x, dim=1)
 
 
 def train_and_test(dataset, device):
-    model = GCN(dataset['data']).to(device)
-    data = dataset['data'][0].to(device)
-    optimizer = torch.optim.Adam(model.parameters(), lr=0.01, weight_decay=5e-4)
+    model_names = ['gcn', 'gat']
+    num_runs = 10
+    print(f"Dataset name: {dataset['data'].name}")
+    for model_name in model_names:
+        accs = []
+        for run_idx in range(num_runs):
+            model = GraphModel(model_name, dataset['data']).to(device)
+            data = dataset['data'][0].to(device)
+            optimizer = torch.optim.Adam(model.parameters(), lr=0.01, weight_decay=5e-4)
 
-    model.train()
-    for epoch in range(200):
-        optimizer.zero_grad()
-        out = model(data)
-        loss = F.nll_loss(out[dataset['train_mask']], data.y[dataset['train_mask']])
-        loss.backward()
-        optimizer.step()
+            model.train()
+            for epoch in range(200):
+                optimizer.zero_grad()
+                out = model(data)
+                loss = F.nll_loss(out[dataset['train_mask']], data.y[dataset['train_mask']])
+                loss.backward()
+                optimizer.step()
 
-    model.eval()
-    pred = model(data).argmax(dim=1)
-    correct = (pred[dataset['test_mask']] == data.y[dataset['test_mask']]).sum()
-    acc = int(correct) / int(dataset['test_mask'].sum())
-    print(f'Accuracy: {acc:.4f}')
+            model.eval()
+            pred = model(data).argmax(dim=1)
+            correct = (pred[dataset['test_mask']] == data.y[dataset['test_mask']]).sum()
+            acc = int(correct) / int(dataset['test_mask'].sum())
+            accs.append(acc)
+
+        print(f'Model: {model_name}')
+        print(f'Average Accuracy: {np.mean(accs):.4f}')
+        print(f'Accuracy Variance: {np.var(accs):.4f}')
 
 def main():
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
